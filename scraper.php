13272e6b8d0980e87945cab555b8bb11554e701c
Jameson Knoll <j.knoll621@gmail.com>
2015-09-19 13:35:43 +0800

    add regex to parse recieved html

--- scraper.php
+++ scraper.php
@@ -1,4 +1,9 @@
 <?php
 	$weather = file_get_contents('http://www.weather-forecast.com/locations/San-Francisco/forecasts/latest');
-	echo($weather);
+
+	#use regex to find '1 – 3 Day Weather Forecast Summary:'
+	preg_match('/3 Day Weather Forecast Summary:/i', $weather, $matches);
+
+	print_r($matches)
+
 ?>
